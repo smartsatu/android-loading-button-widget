a2baa938fd9d81b718c7cbf9cde5a76e0d0782f6
Daniil Lukashevich <danaimset@gmail.com>
2018-08-15 18:28:41 +0300

    Added basic example usage in Kotlin

--- example/src/main/java/com/smartsatu/android/LoadingButtonWidgetDemoActivity.kt
+++ example/src/main/java/com/smartsatu/android/LoadingButtonWidgetDemoActivity.kt
@@ -1,12 +1,20 @@
 package com.smartsatu.android
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_loading_button_widget_demo.*
 
 class LoadingButtonWidgetDemoActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_loading_button_widget_demo)
+
+        loadingButtonWidget.apply {
+            setOnClickListener {
+                startProgress()
+                postDelayed({ stopProgress() }, 2000)
+            }
+        }
     }
 }
