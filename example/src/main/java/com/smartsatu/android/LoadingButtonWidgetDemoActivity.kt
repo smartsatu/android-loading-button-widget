df0f5fecaa68cfef034026e65e66f71560e5e339
Daniil Lukashevich <danaimset@gmail.com>
2020-06-04 15:14:15 +0300

    [example] Material components and with icon in button

--- example/src/main/java/com/smartsatu/android/LoadingButtonWidgetDemoActivity.kt
+++ example/src/main/java/com/smartsatu/android/LoadingButtonWidgetDemoActivity.kt
@@ -3,14 +3,16 @@ package com.smartsatu.android
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import com.smartsatu.android.databinding.ActivityLoadingButtonWidgetDemoBinding
+import com.smartsatu.android.widget.LoadingButtonWidget
 import kotlinx.android.synthetic.main.activity_loading_button_widget_demo.*
 
 class LoadingButtonWidgetDemoActivity : AppCompatActivity() {
 
     private val viewModel by lazy {
-        ViewModelProviders.of(this).get(LoadingButtonWidgetViewModel::class.java)
+        ViewModelProvider(this).get(LoadingButtonWidgetViewModel::class.java)
     }
 
     private lateinit var binding: ActivityLoadingButtonWidgetDemoBinding
@@ -18,14 +20,7 @@ class LoadingButtonWidgetDemoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_loading_button_widget_demo)
-        binding.setLifecycleOwner(this)
+        binding.lifecycleOwner = this
         binding.viewModel = viewModel
-
-        loadingButtonWidget.apply {
-            setOnClickListener {
-                viewModel.progress.value = true
-
-            }
-        }.postDelayed({ viewModel.progress.value = false }, 2000)
     }
 }
--- example/src/main/java/com/smartsatu/android/LoadingButtonWidgetViewModel.kt
+++ example/src/main/java/com/smartsatu/android/LoadingButtonWidgetViewModel.kt
@@ -1,10 +1,24 @@
 package com.smartsatu.android
 
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class LoadingButtonWidgetViewModel : ViewModel() {
 
-    val progress = MutableLiveData<Boolean>()
-            .apply { this.value = false }
+    private val progressInternal = MutableLiveData<Boolean>()
+    val progress = Transformations.map(progressInternal) { it }
+
+    fun showProgress(seconds: Int? = null) {
+        progressInternal.value = true
+        seconds?.let {
+            viewModelScope.launch {
+                delay(seconds * 1000L)
+                progressInternal.value = false
+            }
+        }
+    }
 }
